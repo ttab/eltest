2cc4c670ca73ee23af9c7dd82359cb2b84d03f7e
Hugo Wetterberg <hugo@wetterberg.nu>
2025-09-09 07:30:56 +0200

    add tests for network methods

---
+++ network_test.go
@@ -0,0 +1,23 @@
+package eltest_test
+
+import (
+	"testing"
+
+	"github.com/ttab/eltest"
+)
+
+func TestGetNetworkAndGatewasy(t *testing.T) {
+	network, err := eltest.GetNetwork()
+	eltest.Must(t, err, "get network")
+
+	if network.Name == "" || network.ID == "" {
+		t.Fatal("network must have a name and ID")
+	}
+
+	gw, err := eltest.GetGatewayIP()
+	eltest.Must(t, err, "get gateway IP")
+
+	if len(gw) == 0 {
+		t.Fatal("a valid IP must be returned")
+	}
+}
